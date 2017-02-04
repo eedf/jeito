a9a808a3492811ac61be841ada89b02de96a2cfb
Gael UTARD <gael.utard@midipy.eedf.fr>
2017-02-04 20:47:29 +0100

    Cleanup deprecated app becours

--- becours/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BecoursConfig(AppConfig):
-    name = 'becours'
--- jeito/settings.py
+++ jeito/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'members',
     'docs',
     'booking',
-    'becours',
 ]
 
 MIDDLEWARE_CLASSES = [
