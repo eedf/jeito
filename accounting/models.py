2df9db6889e268bfeea24178e8d73d67175b1f07
Gael UTARD <gael.utard@kisis.fr>
2017-02-07 22:41:55 +0100

    Fix done computation for Budget model

--- accounting/models.py
+++ accounting/models.py
@@ -45,7 +45,7 @@ class Budget(models.Model):
     comment = models.CharField(verbose_name="Commentaire", max_length=1000, blank=True)
 
     def done(self):
-        qs = self.analytic.entry_set.aggregate(amount=models.Sum(models.F('expense') - models.F('revenue')))
+        qs = self.analytic.entry_set.aggregate(amount=models.Sum(models.F('revenue') - models.F('expense')))
         return qs['amount']
 
     def diff(self):
