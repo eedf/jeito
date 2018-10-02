98c016af5b48451ed00058264b5a3a3177e6c5bf
Gaël UTARD <gael.utard@makina-corpus.com>
2018-10-02 18:37:46 +0200

    Report list and create view

--- life/views.py
+++ life/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from .models import Report
 
-# Create your views here.
+
+class ReportListView(ListView):
+    model = Report
+
+
+class ReportCreateView(CreateView):
+    model = Report
+    fields = ('structure', 'date', 'location', 'nb_presents', 'nb_voters', 'representative', 'favour', 'against',
+              'abstention', 'balance', 'budget', 'delegate', 'alternate', 'responsible', 'treasurer',
+              'responsible_validation', 'representative_validation', 'comments_activity_report', 'comments_finances',
+              'comments_national', 'comments_regional', 'comments_problems', 'team')
